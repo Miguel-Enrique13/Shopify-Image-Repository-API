873bbe8ade945de5a1d2cd46246b338f132661ef
Miguel Guardia <guardia@ualberta.ca>
2021-01-17 15:20:42 -0700

    Create "create user" Endpoint with testing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_request
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { message: 'User created successfully' }, status: :ok
+    else
+      render json: { message: user.errors.full_messages.to_sentence }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end
